4805a3202a58bdf414a1827783825357ec3f55b1
Temabiceps <artemkukushkin2002@yahoo.com>
2026-01-31 22:12:42 +0100

    2 modificacion

--- app/Models/FacturaLinea.php
+++ app/Models/FacturaLinea.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class FacturaLinea extends Model
 {
     protected $table = 'facturalineas';
-
+//nuevo commit
     protected $fillable = [
         'id_factura',
         'codigo',
